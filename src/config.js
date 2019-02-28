5bd16b0ef172a2fabfd593d562e5dc9716bf6245
Augusto Samamé <augustosamame@gmail.com>
2019-02-28 04:01:35 -0500

    swithced to production api

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const ENDPOINT = 'http://localhost:3001/api/v1';
-// const ENDPOINT = 'https://troly-api.herokuapp.com/api/v1';
+// const ENDPOINT = 'http://localhost:3001/api/v1';
+const ENDPOINT = 'https://troly-api.herokuapp.com/api/v1';
 
 export {
   ENDPOINT,
